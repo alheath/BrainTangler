6f9e84dd994d078f5fe222166a806ee6a979fcad
Jason <Jason@10.161.64.57>
2014-10-23 17:44:58 -0500

    Test

--- src/projectapp/braintangler/MainActivity.java
+++ src/projectapp/braintangler/MainActivity.java
@@ -2,6 +2,7 @@ package projectapp.braintangler;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -12,6 +13,9 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        
+       Log.d("Testing...", "1...2...3...");
     }
 
 
