dd96da0b940d4014b37035dec6639d188248664d
Alexander <alexander.heath@ttu.edu>
2014-10-24 14:18:12 -0500

    untrack /libs folder

--- src/projectapp/braintangler/MainActivity.java
+++ src/projectapp/braintangler/MainActivity.java
@@ -2,7 +2,6 @@ package projectapp.braintangler;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -13,7 +12,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("test", "this is a test");
     }
 
 
